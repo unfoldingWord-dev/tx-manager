b0a8254ba510d102100b98592ac8fe36d9d9369c
Bruce McLean <mcleanb@photo-nomad.net>
2017-01-25 11:50:58 -0500

    Fix unittest in travis - test_manager.

--- tests/manager_tests/test_manager.py
+++ tests/manager_tests/test_manager.py
@@ -3,7 +3,7 @@ import unittest
 import mock
 import responses
 from six import StringIO
-from tests.manager_tests import mock_utils
+import mock_utils
 from manager.job import TxJob
 from manager.manager import TxManager
 from manager.module import TxModule
