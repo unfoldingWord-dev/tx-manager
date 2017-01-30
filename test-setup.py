932c41a7c9b66d04ef3836814cdfa09842efe1b0
Bruce McLean <mcleanb@photo-nomad.net>
2017-01-30 09:52:49 -0500

    Updated requests version to match version required on travis.

--- test-setup.py
+++ test-setup.py
@@ -12,12 +12,12 @@ setup(
     long_description='Unit test setup file',
     classifiers=[],
     install_requires=[
-        'requests==2.12.5',
+        'requests==2.13.0',
         'responses==0.5.1',
         'boto3==1.4.4',
         'bs4==0.0.1',
         'gogs_client==1.0.3',
-        'mock',
+        'mock', # travis reports syntax error in mock setup.cfg if we give version
         'coveralls==1.1'
     ],
     test_suite='tests'
