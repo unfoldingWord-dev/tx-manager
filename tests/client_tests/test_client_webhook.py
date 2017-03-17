d980fb5a78837241bc952fba7b98759713b13c4f
bspidel <bruce.spidel@gmail.com>
2017-03-17 00:17:43 -0400

    test for travis

--- tests/client_tests/test_client_webhook.py
+++ tests/client_tests/test_client_webhook.py
@@ -8,12 +8,12 @@ from mock import patch
 from client.client_webhook import ClientWebhook
 
 class TestClientWebhook(unittest.TestCase):
-    #baseTemp = "/tmp/repo"
+    baseTemp = "/tmp/repo"
 
-    #try:
-    #    os.makedirs(baseTemp)
-    #except:
-    #    pass
+    try:
+        os.makedirs(baseTemp)
+    except:
+        pass
 
     #tempfile.tempdir = baseTemp
     resources_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'resources')
