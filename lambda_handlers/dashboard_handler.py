530d46143bf991a972ab1d6e75eee4f02b2122b3
Richard Mahn <richard_mahn@wycliffeassociates.org>
2017-03-25 16:20:42 +0000

    Don't require api_url for dashboard

--- lambda_handlers/dashboard_handler.py
+++ lambda_handlers/dashboard_handler.py
@@ -17,7 +17,5 @@ class DashboardHandler(Handler):
         if 'body-json' in event and isinstance(event['body-json'], dict):
             data.update(event['body-json'])
         # Set required env_vars
-        env_vars = {
-            'api_url': self.retrieve(event['vars'], 'api_url', 'Environment Vars')
-        }
+        env_vars = {}
         return TxManager(**env_vars).generate_dashboard()
