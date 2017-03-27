9c4de3b59591dc58f395033b7d32241b48c66005
Richard Mahn <richard_mahn@wycliffeassociates.org>
2017-03-27 23:49:19 +0000

    Stage environment variables needed for dashboard to read proper tables

--- lambda_handlers/dashboard_handler.py
+++ lambda_handlers/dashboard_handler.py
@@ -17,5 +17,11 @@ class DashboardHandler(Handler):
         if 'body-json' in event and isinstance(event['body-json'], dict):
             data.update(event['body-json'])
         # Set required env_vars
-        env_vars = {}
+        env_vars = {
+            'api_url': self.retrieve(event['vars'], 'api_url', 'Environment Vars'),
+            'gogs_url': self.retrieve(event['vars'], 'gogs_url', 'Environment Vars'),
+            'cdn_url': self.retrieve(event['vars'], 'cdn_url', 'Environment Vars'),
+            'job_table_name': self.retrieve(event['vars'], 'job_table_name', 'Environment Vars'),
+            'module_table_name': self.retrieve(event['vars'], 'module_table_name', 'Environment Vars')
+        }
         return TxManager(**env_vars).generate_dashboard()
