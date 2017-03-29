aa80faed77590d00f7b44076dbe66b4c5eb643f0
Bruce McLean <mcleanb@photo-nomad.net>
2017-03-29 16:08:50 -0400

    cleanup.

--- tests/door43_tools_tests/test_usfm_preprocessor.py
+++ tests/door43_tools_tests/test_usfm_preprocessor.py
@@ -48,11 +48,35 @@ class TestUsfmPreprocessor(unittest.TestCase):
         #then
         self.verifyTransform(folder, expectedOutput)
 
+    @classmethod
+    def extractObsFiles(self, file_name, repo_name):
+        file_path = os.path.join(TestUsfmPreprocessor.resources_dir, file_name)
+
+        # 1) unzip the repo files
+        temp_dir = tempfile.mkdtemp(prefix='repo_')
+        unzip(file_path, temp_dir)
+        repo_dir = os.path.join(temp_dir, repo_name)
+        if not os.path.isdir(repo_dir):
+            repo_dir = file_path
+
+        # 2) Get the manifest file or make one if it doesn't exist based on meta.json, repo_name and file extensions
+        manifest_path = os.path.join(repo_dir, 'manifest.json')
+        if not os.path.isfile(manifest_path):
+            manifest_path = os.path.join(repo_dir, 'project.json')
+            if not os.path.isfile(manifest_path):
+                manifest_path = None
+        meta_path = os.path.join(repo_dir, 'meta.json')
+        meta = None
+        if os.path.isfile(meta_path):
+            meta = MetaData(meta_path)
+        manifest = Manifest(file_name=manifest_path, repo_name=repo_name, files_path=repo_dir, meta=meta)
+        return manifest, repo_dir, temp_dir
+
     def runTsUsfmMarkdownPreprocessor(self, manifest, repo_dir):
-        self.out_dir = tempfile.mkdtemp(prefix='output_')
-        compiler = TsBibleUsfmPreprocessor(manifest, repo_dir, self.out_dir)
-        compiler.run()
-        return self.out_dir
+            self.out_dir = tempfile.mkdtemp(prefix='output_')
+            compiler = TsBibleUsfmPreprocessor(manifest, repo_dir, self.out_dir)
+            compiler.run()
+            return self.out_dir
 
     def verifyTransform(self, folder, expectedName):
         file_name = os.path.join(folder, expectedName)
