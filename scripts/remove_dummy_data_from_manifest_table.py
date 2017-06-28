9595b1de1aab7df991565b5d8105af6f40d565cf
Richard Mahn <richard_mahn@wycliffeassociates.org>
2017-06-28 08:47:42 -0600

    Scripts to add and remove dummy data from manifest table

--- scripts/remove_dummy_data_from_manifest_table.py
+++ scripts/remove_dummy_data_from_manifest_table.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Removes dummy data from manifest file starting from user1 repo1 until it can't find one in sequence
+Removes dummy data from manifest table starting from user1 repo1 until it can't find one in sequence
 
 """
 from __future__ import unicode_literals, print_function
