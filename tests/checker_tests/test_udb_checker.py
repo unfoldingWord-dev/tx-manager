5472d259e86694f06ed541b417f71c161389b6a1
Bruce McLean <mcleanb@photo-nomad.net>
2017-08-17 15:17:17 -0400

    verifyUSFM - added checking of untranslated book names in title TestUsfmChecker - added untranslated test cases.

--- tests/checker_tests/test_udb_checker.py
+++ tests/checker_tests/test_udb_checker.py
@@ -4,11 +4,13 @@ import unittest
 import tempfile
 import shutil
 from libraries.checkers.udb_checker import UdbChecker
+from libraries.general_tools.file_utils import unzip
 
 
 class TestUdbChecker(unittest.TestCase):
 
     resources_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources')
+    converter_resources_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../converter_tests/resources')
 
     def setUp(self):
         """Runs before each test."""
@@ -20,13 +22,24 @@ class TestUdbChecker(unittest.TestCase):
         """Runs after each test."""
         shutil.rmtree(self.temp_dir, ignore_errors=True)
 
-    def test_success(self):
+    def test_PhpValid(self):  # for now use same checking as for ulb
+        out_dir = self.unzip_resource('51-PHP.zip')
         expected_warnings = False
         expected_errors = False
-        checker = UdbChecker(self.preconvert_dir, self.converted_dir)
+        checker = UdbChecker(out_dir, self.converted_dir)
         checker.run()
         self.verify_results(expected_errors, expected_warnings, checker)
+
+    #
+    # helpers
+    #
 
     def verify_results(self, expected_errors, expected_warnings, checker):
         self.assertEqual(len(checker.log.logs["warning"]) > 0, expected_warnings)
         self.assertEqual(len(checker.log.logs["error"]) > 0, expected_errors)
+
+    def unzip_resource(self, zip_name):
+        zip_file = os.path.join(self.converter_resources_dir, zip_name)
+        out_dir = os.path.join(self.temp_dir, 'checker_test')
+        unzip(zip_file, out_dir)
+        return out_dir
--- tests/checker_tests/test_ulb_checker.py
+++ tests/checker_tests/test_ulb_checker.py
@@ -4,11 +4,13 @@ import unittest
 import tempfile
 import shutil
 from libraries.checkers.ulb_checker import UlbChecker
+from libraries.general_tools.file_utils import unzip
 
 
 class TestUlbChecker(unittest.TestCase):
 
     resources_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources')
+    converter_resources_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../converter_tests/resources')
 
     def setUp(self):
         """Runs before each test."""
@@ -20,13 +22,24 @@ class TestUlbChecker(unittest.TestCase):
         """Runs after each test."""
         shutil.rmtree(self.temp_dir, ignore_errors=True)
 
-    def test_success(self):
+    def test_PhpValid(self):
+        out_dir = self.unzip_resource('51-PHP.zip')
         expected_warnings = False
         expected_errors = False
-        checker = UlbChecker(self.preconvert_dir, self.converted_dir)
+        checker = UlbChecker(out_dir, self.converted_dir)
         checker.run()
         self.verify_results(expected_errors, expected_warnings, checker)
+
+    #
+    # helpers
+    #
 
     def verify_results(self, expected_errors, expected_warnings, checker):
         self.assertEqual(len(checker.log.logs["warning"]) > 0, expected_warnings)
         self.assertEqual(len(checker.log.logs["error"]) > 0, expected_errors)
+
+    def unzip_resource(self, zip_name):
+        zip_file = os.path.join(self.converter_resources_dir, zip_name)
+        out_dir = os.path.join(self.temp_dir, 'checker_test')
+        unzip(zip_file, out_dir)
+        return out_dir
