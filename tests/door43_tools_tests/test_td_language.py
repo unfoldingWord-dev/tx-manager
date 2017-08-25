05e3412f11472ff77fa114fb1c5225e63c6e2bc1
Richard Mahn <rich.mahn@unfoldingword.org>
2017-08-24 23:40:44 -0600

    Fixes test_td_languages.py

--- tests/door43_tools_tests/test_td_language.py
+++ tests/door43_tools_tests/test_td_language.py
@@ -6,14 +6,14 @@ from libraries.door43_tools.td_language import TdLanguage
 
 
 class TdLanguageTest(unittest.TestCase):
+    def test_init(self):
+        language = TdLanguage({'lc': 'test', 'ln': 'Test Language', 'ld': 'rtl'})
+        self.assertEqual(language.ln, 'Test Language')
+        self.assertEqual(language.ld, 'rtl')
 
-    def setUp(self):
-        """Runs before each test."""
-
-        # Patch _get_url in url_utils for every test case so that it never fetches the actual language json
-        self.patcher = mock.patch('libraries.general_tools.url_utils._get_url')
-        mock_get_url = self.patcher.start()
-        self.languages = [
+    @mock.patch('libraries.general_tools.url_utils._get_url')
+    def test_get_languages(self, mock_get_url):
+        expected_languages = [
             {'gw': False, 'ld': 'ltr', 'ang': 'Afar', 'lc': 'aa', 'ln': 'Afaraf', 'lr': 'Africa', 'pk': 6},
             {'gw': True, 'ld': 'ltr', 'ang': 'English', 'lc': 'en', 'ln': 'English',
              'lr': 'Europe', 'pk': 1747},
@@ -22,21 +22,10 @@ class TdLanguageTest(unittest.TestCase):
             {'gw': True, 'ld': 'ltr', 'ang': 'French', 'lc': 'fr', 'ln': 'fran\xe7ais, langue fran\xe7aise',
              'lr': 'Europe', 'pk': 1868}
         ]
-        mock_get_url.return_value = json.dumps(self.languages)
-
-    def tearDown(self):
-        """Runs after each test."""
-        self.patcher.stop()
-
-    def test_init(self):
-        language = TdLanguage({'lc': 'test', 'ln': 'Test Language', 'ld': 'rtl'})
-        self.assertEqual(language.ln, 'Test Language')
-        self.assertEqual(language.ld, 'rtl')
-
-    def test_get_languages(self):
+        mock_get_url.return_value = json.dumps(expected_languages)
         languages = TdLanguage.get_languages()
         self.assertTrue('fr' in languages)
-        self.assertEqual(len(languages), len(self.languages))
+        self.assertEqual(len(languages), len(expected_languages))
 
     def test_get_language(self):
         language = TdLanguage.get_language('aa')
