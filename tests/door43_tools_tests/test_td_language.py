e2cf4a6a0310e464535a186760a7240d883ab9ae
Richard Mahn <richard_mahn@wycliffeassociates.org>
2017-08-25 17:10:56 +0000

    Fix for td_language test

--- tests/door43_tools_tests/test_td_language.py
+++ tests/door43_tools_tests/test_td_language.py
@@ -6,31 +6,38 @@ from libraries.door43_tools.td_language import TdLanguage
 
 
 class TdLanguageTest(unittest.TestCase):
-    languages = [
-        {'gw': False, 'ld': 'ltr', 'ang': 'Afar', 'lc': 'aa', 'ln': 'Afaraf', 'lr': 'Africa', 'pk': 6},
-        {'gw': True, 'ld': 'ltr', 'ang': 'English', 'lc': 'en', 'ln': 'English',
-         'lr': 'Europe', 'pk': 1747},
-        {'gw': True, 'ld': 'ltr', 'ang': 'Spanish', 'lc': 'es', 'ln': 'espa\xf1ol',
-         'lr': 'Europe', 'pk': 1776},
-        {'gw': True, 'ld': 'ltr', 'ang': 'French', 'lc': 'fr', 'ln': 'fran\xe7ais, langue fran\xe7aise',
-         'lr': 'Europe', 'pk': 1868}
-    ]
+    def setUp(self):
+        """Runs before each test."""
+
+        # Patch _get_url in url_utils for every test case so that it never fetches the actual language json
+        self.patcher = mock.patch('libraries.general_tools.url_utils._get_url')
+        mock_get_url = self.patcher.start()
+        self.languages = [
+            {'gw': False, 'ld': 'ltr', 'ang': 'Afar', 'lc': 'aa', 'ln': 'Afaraf', 'lr': 'Africa', 'pk': 6},
+            {'gw': True, 'ld': 'ltr', 'ang': 'English', 'lc': 'en', 'ln': 'English',
+             'lr': 'Europe', 'pk': 1747},
+            {'gw': True, 'ld': 'ltr', 'ang': 'Spanish', 'lc': 'es', 'ln': 'espa\xf1ol',
+             'lr': 'Europe', 'pk': 1776},
+            {'gw': True, 'ld': 'ltr', 'ang': 'French', 'lc': 'fr', 'ln': 'fran\xe7ais, langue fran\xe7aise',
+             'lr': 'Europe', 'pk': 1868}
+        ]
+        mock_get_url.return_value = json.dumps(self.languages)
+
+    def tearDown(self):
+        """Runs after each test."""
+        self.patcher.stop()
 
     def test_init(self):
         language = TdLanguage({'lc': 'test', 'ln': 'Test Language', 'ld': 'rtl'})
         self.assertEqual(language.ln, 'Test Language')
         self.assertEqual(language.ld, 'rtl')
 
-    @mock.patch('libraries.general_tools.url_utils._get_url')
-    def test_get_languages(self, mock_get_url):
-        mock_get_url.return_value = json.dumps(self.languages)
+    def test_get_languages(self):
         languages = TdLanguage.get_languages()
         self.assertTrue('fr' in languages)
         self.assertEqual(len(languages), len(self.languages))
 
-    @mock.patch('libraries.general_tools.url_utils._get_url')
-    def test_get_language(self, mock_get_url):
-        mock_get_url.return_value = json.dumps(self.languages)
+    def test_get_language(self):
         language = TdLanguage.get_language('aa')
         self.assertIsNotNone(language)
         self.assertEqual(language.ln, 'Afaraf')
