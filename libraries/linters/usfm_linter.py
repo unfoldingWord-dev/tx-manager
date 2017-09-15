586035a3984575c5c8f214c7c23d9b9118e8d682
bspidel <bruce.spidel@gmail.com>
2017-09-15 10:15:22 -0400

    place guard around split

--- libraries/linters/usfm_linter.py
+++ libraries/linters/usfm_linter.py
@@ -84,8 +84,11 @@ class UsfmLinter(Linter):
             if len(errors):
                 for error in errors:
                     parts = error.split( ":" )
-                    self.log.warning( "{0} {1} - {2}".format(book_code, " ".join( parts[1:] ), parts[0] ))
 
+                    if( parts.length < 2 ):
+                        parts.push( "")
+
+                    self.log.warning( "{0} {1} - {2}".format(book_code, " ".join( parts[1:] ), parts[0] ))
         except Exception as e:
             # for debugging
             self.log.warning("Failed to verify book '{0}', exception: {1}".format(file_name, str(e)))
