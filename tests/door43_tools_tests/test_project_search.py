959716559f84c7395750ed1fb729f8ac1fe0c9b5
Bruce McLean <mcleanb@photo-nomad.net>
2017-09-21 10:14:01 -0400

    test cleanup.

--- tests/door43_tools_tests/test_project_search.py
+++ tests/door43_tools_tests/test_project_search.py
@@ -36,7 +36,7 @@ class ProjectSearchTest(unittest.TestCase):
                 'resource_type': 'book',
                 'title': 'Open Bible Stories',
                 'views': 3,
-                'last_updated': datetime.utcnow() - timedelta(months=11),
+                'last_updated': self.get_time_n_months_back(11),
                 'manifest': {'description': 'French Bible Stories'},
             },
             'francis/fr_ulb': {
@@ -47,7 +47,7 @@ class ProjectSearchTest(unittest.TestCase):
                 'resource_type': 'bundle',
                 'title': 'Unlocked Literal Bible',
                 'views': 12,
-                'last_updated': datetime.utcnow() - timedelta(months=6),
+                'last_updated': self.get_time_n_months_back(6),
                 'manifest': {'description': 'French Unlocked Literal Bible'},
             },
         }
@@ -204,6 +204,31 @@ class ProjectSearchTest(unittest.TestCase):
         self.assertEqual(len(results), 2)
         self.assertEqual(search.url_params, '?resType=book')
 
+    def test_apply_filters(self):
+        query = TxManifest.query()
+        search = ProjectSearch()
+        search.url_params = ""
+        min_views_query = search.apply_filters(query, 'minViews', 10)
+        self.assertEqual(min_views_query.count(), 1)  # count is the number of entries matched
+        days_for_recent_query = search.apply_filters(query, 'daysForRecent', 30)
+        self.assertEqual(days_for_recent_query.count(), 1)
+        repo_query = search.apply_filters(query, 'repo_name', 'obs')
+        self.assertEqual(repo_query.count(), 2)
+        user_query = search.apply_filters(query, 'user_name', 'john')
+        self.assertEqual(user_query.count(), 1)
+        title_query = search.apply_filters(query, 'title', 'Open')
+        self.assertEqual(title_query.count(), 2)
+        full_text_query = search.apply_filters(query, 'full_text', '%great%')
+        self.assertEqual(full_text_query.count(), 1)
+        last_updated_query = search.apply_filters(query, 'time', str(datetime.utcnow().year))
+        self.assertGreaterEqual(last_updated_query.count(), 1)
+        resource_id_query = search.apply_filters(query, 'resID', 'obs')
+        self.assertEqual(resource_id_query.count(), 2)
+        resource_type_query = search.apply_filters(query, 'resType', 'bundle')
+        self.assertEqual(resource_type_query.count(), 1)
+        languages_query = search.apply_filters(query, 'languages', "[fr,es]")
+        self.assertEqual(languages_query.count(), 2)
+
     #
     # helpers
     #
@@ -216,26 +241,8 @@ class ProjectSearchTest(unittest.TestCase):
     def mock_save_url_search(self):
         pass
 
-    def test_apply_filters(self):
-        query = TxManifest.query()
-        search = ProjectSearch()
-        min_views_query = search.apply_filters(query, 'minViews', 10)
-        self.assertEqual(min_views_query.count(), 1)
-        days_for_recent_query = search.apply_filters(query, 'daysForRecent', 30)
-        self.assertEqual(days_for_recent_query.count(), 2)
-        repo_query = search.apply_filters(query, 'repo_name', 'obs')
-        self.assertEqual(repo_query.count(), 2)
-        user_query = search.apply_filters(query, 'user_name', 'john')
-        self.assertEqual(user_query.count(), 1)
-        title_query = search.apply_filters(query, 'title', 'Open')
-        self.assertEqual(title_query.count(), 2)
-        full_text_query = search.apply_filters(query, 'full_text', '%great%')
-        self.assertEqual(full_text_query.count(), 1)
-        last_updated_query = search.apply_filters(query, 'time', '2016')
-        self.assertEqual(last_updated_query.count(), 1)
-        resource_id_query = search.apply_filters(query, 'resID', 'obs')
-        self.assertEqual(resource_id_query.count(), 2)
-        resource_type_query = search.apply_filters(query, 'resType', 'bundle')
-        self.assertEqual(resource_type_query.count(), 1)
-        languages_query = search.apply_filters(query, 'languages', "[fr,es]")
-        self.assertEqual(languages_query.count(), 1)
+    def get_time_n_months_back(self, months):
+        current = datetime.now()
+        offset = -months * 30 * 24 * 60 * 60  # in seconds
+        recent_in_seconds = current + timedelta(seconds=offset)
+        return recent_in_seconds
