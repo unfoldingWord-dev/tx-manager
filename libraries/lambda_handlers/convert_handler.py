a3a1184f11348440838d7e12b1024d39a9599fc8
Bruce McLean <mcleanb@photo-nomad.net>
2017-09-21 15:34:58 -0400

    ConvertHandler - tweaks for testing TestConvertHandler - adding tests

--- libraries/lambda_handlers/convert_handler.py
+++ libraries/lambda_handlers/convert_handler.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals, print_function
+import json
 import requests
 from libraries.app.app import App
 from libraries.lambda_handlers.handler import Handler
@@ -18,6 +19,8 @@ class ConvertHandler(Handler):
             args = list(args)
             self.converter_class = args.pop()
         super(ConvertHandler, self).__init__()
+        self.callback_status = 0
+        self.callback_payload = None
 
     def _handle(self, event, context):
         """
@@ -30,7 +33,7 @@ class ConvertHandler(Handler):
         source = self.retrieve(job, 'source', 'job')
         resource = self.retrieve(job, 'resource_type', 'job')
         cdn_file = self.retrieve(job, 'cdn_file', 'job')
-        callback = self.retrieve(job, 'callback', 'job', required=False)
+        callback = self.retrieve(self.data, 'convert_callback', 'convert_callback', required=False)
         options = {}
         if 'options' in job:
             options = job['options']
@@ -40,8 +43,9 @@ class ConvertHandler(Handler):
         results = converter.run()
         converter.close()  # do cleanup after run
         if callback is not None:
-            self.data['results'] = results  # add results to payload and call back
-            self.do_callback(callback, self.data)
+            self.callback_payload = json.loads(json.dumps(self.data))  # clone so we can modify
+            self.callback_payload['results'] = results  # add results to payload and call back
+            self.do_callback(callback, self.callback_payload)
         return results
 
     def do_callback(self, url, payload):
@@ -50,10 +54,10 @@ class ConvertHandler(Handler):
             App.logger.debug('Making callback to {0} with payload:'.format(url))
             App.logger.debug(payload)
             response = requests.post(url, json=payload, headers=headers)
-            status = response.status_code
-            if (status >= 200) and (status < 299):
+            self.callback_status = response.status_code
+            if (self.callback_status >= 200) and (self.callback_status < 299):
                 App.logger.debug('finished.')
             else:
-                App.logger.error('Error calling callback code {0}: {1}'.format(status, response.reason))
+                App.logger.error('Error calling callback code {0}: {1}'.format(self.callback_status, response.reason))
         else:
             App.logger.error('Invalid callback url: {0}'.format(url))
