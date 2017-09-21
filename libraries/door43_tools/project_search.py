9d344d951a0729433b1251d761f7195b31c2de6d
Richard Mahn <rich.mahn@unfoldingword.org>
2017-09-20 22:34:19 -0600

    Fixes for tests

--- libraries/door43_tools/project_search.py
+++ libraries/door43_tools/project_search.py
@@ -25,30 +25,30 @@ class ProjectSearch(object):
         self.criterion = json.loads(json.dumps(criterion))  # clone so we can modify
 
         try:
-            selection = TxManifest.query()
+            query = TxManifest.query()
 
             for k in self.criterion:
                 v = self.criterion[k]
-                selection = self.apply_filters(selection, k, v)
-                if selection is None:
+                query = self.apply_filters(query, k, v)
+                if query is None:
                     return None
 
             if 'sort_by' in self.criterion:
                 db_key = getattr(TxManifest, self.criterion['sort_by'], None)
                 if db_key:
-                    selection = selection.order_by(db_key)
+                    query = query.order_by(db_key)
 
             if 'sort_by_reversed' in self.criterion:
                 db_key = getattr(TxManifest, self.criterion['sort_by_reversed'], None)
                 if db_key:
-                    selection = selection.order_by(db_key.desc())
+                    query = query.order_by(db_key.desc())
 
         except Exception as e:
             self.log_error('Failed to create a query: ' + str(e))
             return None
 
         limit = 100 if 'matchLimit' not in self.criterion else self.criterion['matchLimit']
-        results = selection.limit(limit).all()  # get all matching
+        results = query.limit(limit).all()  # get all matching
         data = []
         if results:
             App.logger.debug('Returning search result count of {0}')
@@ -73,30 +73,36 @@ class ProjectSearch(object):
 
         return data
 
-    def apply_filters(self, selection, key, value):
+    def apply_filters(self, query, key, value):
         try:
             if key == "minViews":
-                selection = selection.filter(TxManifest.views >= parse_int(value, 1))
+                query = query.filter(TxManifest.views >= parse_int(value, 1))
             elif key == "daysForRecent":
                 days = parse_int(value, 1)
                 current = datetime.datetime.now()
                 offset = -days * 24 * 60 * 60  # in seconds
                 recent_in_seconds = current + datetime.timedelta(seconds=offset)
-                selection = selection.filter(TxManifest.last_updated >= recent_in_seconds)
-            elif (key == "repo_name") or (key == "user_name") or (key == "title") or (key == "manifest"):
-                selection = set_contains_string_filter(selection, key, value)
+                query = query.filter(TxManifest.last_updated >= recent_in_seconds)
+            elif key == 'repo_name':
+                query = query.filter(TxManifest.repo_name.contains(value))
+            elif key == 'user_name':
+                query = query.filter(TxManifest.user_name.contains(value))
+            elif key == 'title':
+                query = query.filter(TxManifest.title.contains(value))
+            elif key == 'manifest':
+                query = query.filter(TxManifest.manifest.contains(value))
             elif key == "time":
-                selection = set_contains_string_filter(selection, "last_updated", value)
+                query = query.filter(TxManifest.last_updated.contains(value))
             elif key == "resID":
-                selection = selection.filter(TxManifest.resource_id.contains(value))
+                query = query.filter(TxManifest.resource_id.contains(value))
             elif key == "resType":
-                selection = selection.filter(TxManifest.resource_type.contains(value))
+                query = query.filter(TxManifest.resource_type.contains(value))
             elif key == "languages":
-                selection = set_contains_set_filter(selection, "lang_code", value)
+                query = set_contains_set_filter(query, "lang_code", value)
             elif key == 'full_text':
-                selection = selection.filter((TxManifest.user_name.contains(value))
-                                             | (TxManifest.repo_name.contains(value))
-                                             | (TxManifest.manifest.contains(value)))
+                query = query.filter((TxManifest.user_name.contains(value))
+                                     | (TxManifest.repo_name.contains(value))
+                                     | (TxManifest.manifest.contains(value)))
             elif key == "returnedFields" or key == "sort_by" or key == "sort_by_reversed" or key == "matchLimit":
                 pass  # skip this item
             else:
@@ -106,29 +112,23 @@ class ProjectSearch(object):
             self.log_error('Failed to apply filter (key,value): ({0},{1}): '.format(key, value) + str(e))
             return None
 
-        return selection
+        return query
 
     def log_error(self, msg):
         self.error = msg
         App.logger.debug(msg)
 
 
-def set_contains_string_filter(selection, key, value):
-    db_key = getattr(TxManifest, key, None)
-    selection = selection.filter(db_key.contains(value))
-    return selection
-
-
-def set_contains_set_filter(selection, key, value):
+def set_contains_set_filter(query, key, value):
     db_key = getattr(TxManifest, key, None)
     if value[:1] == '[':
         filter_set_str = value[1:].split(']')
         filter_set = filter_set_str[0].split(',')
-        selection = selection.filter(db_key.in_(filter_set))
+        query = query.filter(db_key.in_(filter_set))
     else:
-        selection = selection.filter(db_key.like(value))
+        query = query.filter(db_key.like(value))
 
-    return selection
+    return query
 
 
 def parse_int(s, default_value=None):
