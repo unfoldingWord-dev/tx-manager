dbd270092dfcd27cff8689b577f9c0e2eed4d25c
Richard Mahn <rich.mahn@unfoldingword.org>
2017-09-27 15:59:47 -0600

    Fixed unit tests

--- tests/client_tests/test_client_converter_callback.py
+++ tests/client_tests/test_client_converter_callback.py
@@ -51,24 +51,24 @@ class TestClientConverterCallback(TestCase):
         self.items = {
             'job1': {
                 'job_id': 'job1',
-                'identifier': 'user1/repo1/commit1',
-                'owner_name': 'user1',
-                'repo_name': 'repo1',
-                'commit_id': 'commit1',
+                'identifier': 'tx-manager-test-data/en-ulb/22f3d09f7a',
+                'owner_name': 'tx-manager-test-data',
+                'repo_name': 'en-ulb',
+                'commit_id': '22f3d09f7a',
                 'user': 'user1',
                 'status': 'started',
                 'resource_type': 'obs',
-                'input_format': 'md',
+                'input_format': 'usfm',
                 'output_format': 'html',
-                'convert_module': 'convert_md2html',
+                'convert_module': 'convert_usfm2html',
                 'created_at': datetime.utcnow(),
                 'updated_at': datetime.utcnow(),
                 'expires_at': datetime.utcnow() + timedelta(days=1),
                 'started_at': None,
                 'ended_at': None,
                 'eta': datetime.utcnow() + timedelta(minutes=5),
-                'source': 'https://some/url',
-                'output': 'u/user1/repo1',
+                'output': 'https://test-cdn.door43.org/tx-manager-test-data/en-ulb-jud/6778aa89bd.zip',
+                'source': 'https://s3-us-west-2.amazonaws.com/tx-webhook-client/preconvert/e8eb91750d.zip',
                 'cdn_bucket': 'cdn.door43.org',
                 'cdn_file': 'u/user1/repo1',
                 'callback': 'https://client/callback',
@@ -76,7 +76,7 @@ class TestClientConverterCallback(TestCase):
                 'success': False,
                 'message': 'Started',
                 'log': ['Started job'],
-                'warnings': ['Linter warning'],
+                'warnings': ['warning'],
                 'errors': []
             },
             'job2': {
@@ -86,11 +86,11 @@ class TestClientConverterCallback(TestCase):
                 'repo_name': 'en-ulb-jud',
                 'commit_id': '22f3d09f7a',
                 'user': 'user1',
-                'status': 'requested',
+                'status': 'started',
                 'resource_type': 'obs',
-                'input_format': 'md',
+                'input_format': 'usfm',
                 'output_format': 'html',
-                'convert_module': 'module1',
+                'convert_module': 'convert_usfm2html',
                 'cdn_bucket': 'cdn.door43.org',
                 'output': 'https://test-cdn.door43.org/tx-manager-test-data/en-ulb-jud/6778aa89bd.zip',
                 'source': 'https://s3-us-west-2.amazonaws.com/tx-webhook-client/preconvert/e8eb91750d.zip',
@@ -104,10 +104,10 @@ class TestClientConverterCallback(TestCase):
                 'callback': 'https://client/callback',
                 'links': {'link': 'https://some/link'},
                 'success': False,
-                'message': 'Requested',
-                'log': ['Requestedjob'],
-                'warnings': ['Linter warning'],
-                'errors': ['error']
+                'message': 'Started',
+                'log': ['Started job'],
+                'warnings': ['warning'],
+                'errors': []
             },
             'job3': {
                 'job_id': 'job3',
@@ -136,8 +136,8 @@ class TestClientConverterCallback(TestCase):
                 'success': False,
                 'message': 'Requested',
                 'log': ['Requested job'],
-                'warnings': [],
-                'errors': []
+                'warnings': ['warning'],
+                'errors': ['error']
             },
         }
 
@@ -146,91 +146,91 @@ class TestClientConverterCallback(TestCase):
             tx_job = TxJob(**self.items[idx])
             tx_job.insert()
 
-    @patch('libraries.client.client_callback.download_file')
-    def test_clientCallbackSimpleJob(self, mock_download_file):
+    @patch('libraries.client.client_converter_callback.download_file')
+    def test_client_client_converter_callback_simple_job(self, mock_download_file):
         # given
         self.source_zip = os.path.join(self.resources_dir, "raw_sources/en-ulb.zip")
         identifier = 'job2'
-        mock_ccb = self.mock_client_converter_callback(identifier, mock_download_file)
+        mock_cccb = self.mock_client_converter_callback(identifier, mock_download_file)
         expect_error = False
 
         # when
-        results = mock_ccb.process_callback()
+        results = mock_cccb.process_converter_callback()
 
         # then
         self.validate_results(expect_error, results)
 
-    @patch('libraries.client.client_callback.download_file')
-    def test_clientCallbackMultipleJobPartial(self, mock_download_file):
+    @patch('libraries.client.client_converter_callback.download_file')
+    def test_client_callback_multiple_job_partial(self, mock_download_file):
         # given
         self.source_zip = os.path.join(self.resources_dir, "raw_sources/en-ulb.zip")
-        identifier = 'tx-manager-test-data/en-ulb/22f3d09f7a/2/1/01-GEN.usfm'
+        identifier = 'job1/2/1/01-GEN.usfm'
         self.generate_parts_completed(1, 2)
-        mock_ccb = self.mock_client_converter_callback(identifier, mock_download_file)
+        mock_cccb = self.mock_client_converter_callback(identifier, mock_download_file)
         expect_error = False
 
         # when
-        results = mock_ccb.process_callback()
+        results = mock_cccb.process_converter_callback()
 
         # then
         self.validate_results(expect_error, results)
 
-    @patch('libraries.client.client_callback.download_file')
-    def test_clientCallbackMultipleJobComplete(self, mock_download_file):
+    @patch('libraries.client.client_converter_callback.download_file')
+    def test_client_converter_callback_multiple_job_complete(self, mock_download_file):
         # given
         self.source_zip = os.path.join(self.resources_dir, "raw_sources/en-ulb.zip")
-        identifier = 'tx-manager-test-data/en-ulb/22f3d09f7a/2/0/01-GEN.usfm'
+        identifier = 'job1/2/0/01-GEN.usfm'
         self.generate_parts_completed(0, 2)
-        mock_ccb = self.mock_client_converter_callback(identifier, mock_download_file)
+        mock_cccb = self.mock_client_converter_callback(identifier, mock_download_file)
         expect_error = False
 
         # when
-        results = mock_ccb.process_callback()
+        results = mock_cccb.process_converter_callback()
 
         # then
         self.validate_results(expect_error, results)
 
-    @patch('libraries.client.client_callback.download_file')
-    def test_clientCallbackMultipleJobCompleteError(self, mock_download_file):
+    @patch('libraries.client.client_converter_callback.download_file')
+    def test_client_converter_callback_multiple_job_complete_error(self, mock_download_file):
         # given
         self.source_zip = os.path.join(self.resources_dir, "raw_sources/en-ulb.zip")
-        identifier = 'tx-manager-test-data/en-ulb/22f3d09f7a/2/0/01-GEN.usfm'
+        identifier = 'job1/2/0/01-GEN.usfm'
         self.generate_parts_completed(0, 2)
-        mock_ccb = self.mock_client_converter_callback(identifier, mock_download_file, 'conversion failed')
+        mock_cccb = self.mock_client_converter_callback(identifier, mock_download_file, ['conversion failed'])
         expect_error = True
 
         # when
-        results = mock_ccb.process_callback()
+        results = mock_cccb.process_converter_callback()
 
         # then
         self.validate_results(expect_error, results)
 
-    @patch('libraries.client.client_callback.download_file')
-    def test_clientCallbackMultipleNoJobsComplete(self, mock_download_file):
+    @patch('libraries.client.client_converter_callback.download_file')
+    def test_client_converter_callback_multiple_noo_jobs_complete(self, mock_download_file):
         # given
         self.source_zip = os.path.join(self.resources_dir, "raw_sources/en-ulb.zip")
-        identifier = 'tx-manager-test-data/en-ulb/22f3d09f7a/2/0/01-GEN.usfm'
+        identifier = 'job1/2/0/01-GEN.usfm'
         self.generate_parts_completed(0, 0)
-        mock_ccb = self.mock_client_converter_callback(identifier, mock_download_file)
+        mock_cccb = self.mock_client_converter_callback(identifier, mock_download_file)
         expect_error = False
 
         # when
-        results = mock_ccb.process_callback()
+        results = mock_cccb.process_converter_callback()
 
         # then
         self.validate_results(expect_error, results)
 
-    @patch('libraries.client.client_callback.download_file')
-    def test_clientCallbackDownloadException(self, mock_download_file):
+    @patch('libraries.client.client_converter_callback.download_file')
+    def test_client_converter_callback_download_exception(self, mock_download_file):
         # given
         self.raiseDownloadException = True
         self.source_zip = os.path.join(self.resources_dir, "raw_sources/en-ulb.zip")
-        identifier = 'tx-manager-test-data/en-ulb/22f3d09f7a/01-GEN.usfm'
-        mock_ccb = self.mock_client_converter_callback(identifier, mock_download_file)
+        identifier = 'job1/01-GEN.usfm'
+        mock_cccb = self.mock_client_converter_callback(identifier, mock_download_file)
         expect_error = True
 
         # when
-        results = mock_ccb.process_callback()
+        results = mock_cccb.process_converter_callback()
 
         # then
         self.validate_results(expect_error, results)
@@ -247,8 +247,16 @@ class TestClientConverterCallback(TestCase):
         else:
             self.assertEquals(error_count, 0)
 
-    def mock_client_converter_callback(self, identifier, mock_download_file, error=None):
+    def mock_client_converter_callback(self, identifier, mock_download_file, errors=None):
+        """
+        :param string identifier:
+        :param mock_download_file:
+        :param list errors:
+        :return ClientConverterCallback:
+        """
         mock_download_file.side_effect = self.mock_download_file
+        if not errors:
+            errors = []
         self.build_log_json = {
             'dummy_data': 'stuff',
             'commit_id':  '123456ff',
@@ -256,28 +264,13 @@ class TestClientConverterCallback(TestCase):
             'started_at': '2017-05-22T13:39:16Z',
             'repo_owner': 'repo_owner1',
             'repo_name':  'repo_name2',
-            'resource_type': 'resource_type3'
+            'resource_type': 'resource_type3',
+            'errors': errors
         }
-        if error:
-            self.build_log_json['errors'] = [error]
-
         self.build_log_json = json.dumps(self.build_log_json)
-
         self.project_json = '{}'
-
-        job_data = {
-            'job_id': '123',
-            'created_at': '2017-05-22T13:39:15Z',
-            'identifier': ('%s' % identifier),
-            'output':     'https://test-cdn.door43.org/tx/job/6864ae1b91195f261ba5cda62d58d5ad9333f3131c787bb68f20c27adcc85cad.zip',
-            'ended_at':   '2017-05-22T13:39:17Z',
-            'started_at': '2017-05-22T13:39:16Z',
-            'status':     'started',
-            'success':    'success'
-        }
-
-        ccb = ClientConverterCallback(job_data=job_data)
-        return ccb
+        cccb = ClientConverterCallback(identifier=identifier, success=True, info=[], warnings=[], errors=[])
+        return cccb
 
     def mock_download_file(self, url, target):
         if self.raiseDownloadException:
@@ -292,8 +285,7 @@ class TestClientConverterCallback(TestCase):
             file_utils.write_file(target, self.project_json)
 
     def mock_cdn_upload_file(self, project_file, s3_key, cache_time=600):
-        self.transferred_files.append({'type': 'upload', 'file': project_file,
-                                                    'key': s3_key})
+        self.transferred_files.append({'type': 'upload', 'file': project_file, 'key': s3_key})
         return
 
     def mock_cdn_get_json(self, s3_key):
