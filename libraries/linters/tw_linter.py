2a1159e8503e8b6c30d33304a896c0563054ab09
Bruce McLean <mcleanb@photo-nomad.net>
2017-10-09 08:16:16 -0400

    TwLinter - added checking of unconverted links TestTwLinter - added tests for unconverted links warnings

--- libraries/linters/tw_linter.py
+++ libraries/linters/tw_linter.py
@@ -1,4 +1,7 @@
 from __future__ import print_function, unicode_literals
+import os
+from libraries.app.app import App
+from libraries.general_tools import file_utils
 from libraries.linters.markdown_linter import MarkdownLinter
 
 
@@ -12,4 +15,31 @@ class TwLinter(MarkdownLinter):
         self.source_dir is the directory of source files (.md)
         :return bool:
         """
+        for root, dirs, files in os.walk(self.source_dir):
+            for f in files:
+                file_path = os.path.join(root, f)
+                parts = os.path.splitext(file_path)
+                if parts[1] == '.md':
+                    App.logger.debug("Processing: " + f)
+                    contents = file_utils.read_file(file_path)
+
+                    # find uncoverted links
+                    self.find_unconverted_link(contents, '(../')
+                    self.find_unconverted_link(contents, '(./')
+
         return super(TwLinter, self).lint()  # Runs checks on Markdown, using the markdown linter
+
+    def find_unconverted_link(self, contents, link):
+        pos = contents.find(link)
+        while pos >= 0:
+            link_start = contents.rfind('[', 0, pos)
+            if link_start < 0:
+                link_start = 0
+            line_start = contents.rfind('\n', 0, pos)
+            if line_start < 0:
+                line_start = 0
+            link_start = link_start if link_start > line_start else line_start
+            end = contents.find(')', pos + 1)
+            invalid_link = contents[link_start:end + 1]
+            self.log.warning("Invalid link: {0}".format(invalid_link))
+            pos = contents.find(link, end)
--- tests/linter_tests/test_tw_linter.py
+++ tests/linter_tests/test_tw_linter.py
@@ -3,6 +3,8 @@ import os
 import tempfile
 import shutil
 import mock
+
+from libraries.general_tools.file_utils import read_file, write_file, unzip, add_contents_to_zip
 from tests.linter_tests.linter_unittest import LinterTestCase
 from libraries.linters.tw_linter import TwLinter
 
@@ -28,5 +30,46 @@ class TestTwLinter(LinterTestCase):
         linter.run()
         self.verify_results(expected_warnings, linter)
 
+    @mock.patch('libraries.linters.markdown_linter.MarkdownLinter.invoke_markdown_linter')
+    def test_lint_broken_links(self, mock_invoke_markdown_linter):
+        mock_invoke_markdown_linter.return_value = {}  # Don't care about markdown linting here, just specific tw linting
+        expected_warnings = True
+        zip_file = os.path.join(self.resources_dir, 'tw_linter', 'en_tw.zip')
+        new_zip = self.replace_text(zip_file, 'en_tw', 'names.md', '(#moses)', '(../moses.md)')
+        linter = TwLinter(source_zip_file=new_zip)
+        linter.run()
+        self.verify_results(expected_warnings, linter)
+
+    @mock.patch('libraries.linters.markdown_linter.MarkdownLinter.invoke_markdown_linter')
+    def test_lint_broken_links2(self, mock_invoke_markdown_linter):
+        mock_invoke_markdown_linter.return_value = {}  # Don't care about markdown linting here, just specific tw linting
+        expected_warnings = True
+        zip_file = os.path.join(self.resources_dir, 'tw_linter', 'en_tw.zip')
+        new_zip = self.replace_text(zip_file, 'en_tw', 'names.md', '(#moses)', '(./moses.md)')
+        linter = TwLinter(source_zip_file=new_zip)
+        linter.run()
+        self.verify_results(expected_warnings, linter)
+
     def verify_results(self, expected_warnings, linter):
         self.assertEqual(len(linter.log.warnings) > 0, expected_warnings)
+
+    #
+    # helpers
+    #
+
+    def replace_text(self, zip_name, sub_folder, file_name, match, replace):
+        out_dir = self.unzip_resource(zip_name)
+        file_path = os.path.join(out_dir, sub_folder, file_name)
+        text = read_file(file_path)
+        new_text = text.replace(match, replace)
+        write_file(file_path, new_text)
+
+        new_zip = tempfile.mktemp(prefix="linter", suffix='.zip', dir=self.temp_dir)
+        add_contents_to_zip(new_zip, out_dir)
+        return new_zip
+
+    def unzip_resource(self, zip_name):
+        zip_file = os.path.join(self.resources_dir, zip_name)
+        out_dir = tempfile.mkdtemp(dir=self.temp_dir, prefix='linter_test_')
+        unzip(zip_file, out_dir)
+        return out_dir
