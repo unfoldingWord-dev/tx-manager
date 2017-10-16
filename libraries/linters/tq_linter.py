bdd0e6c05eb2de5a40ce6b29d8b35fac5cba342f
Bruce McLean <mcleanb@photo-nomad.net>
2017-10-16 07:24:17 -0400

    TqLinter & TestTqLinter - improving missing book testing

--- libraries/linters/tq_linter.py
+++ libraries/linters/tq_linter.py
@@ -4,7 +4,6 @@ import os
 
 from libraries.app.app import App
 from libraries.client.preprocessors import TqPreprocessor
-from libraries.general_tools import file_utils
 from libraries.linters.markdown_linter import MarkdownLinter
 
 
@@ -20,27 +19,29 @@ class TqLinter(MarkdownLinter):
         """
         for section in TqPreprocessor.sections:
             book = section['book']
-            file_path = os.path.join(self.source_dir, '{0}.md'.format(book))
-            if os.path.exists(file_path):
-                contents = file_utils.read_file(file_path)
-                App.logger.debug("Book {0} found, length={1}".format(book, len(contents)))
+            if book[:3] == "00-":
                 continue
-            else:
-                found_files = False
-                link = self.get_link_for_book(book)
-                file_path = os.path.join(self.source_dir, link)
-                for root, dirs, files in os.walk(file_path):
-                    if root == file_path:
-                        continue  # skip book folder
 
-                    if len(files) > 0:
+            found_files = False
+            link = self.get_link_for_book(book)
+            file_path = os.path.join(self.source_dir, link)
+            for root, dirs, files in os.walk(file_path):
+                if root == file_path:
+                    continue  # skip book folder
+
+                for file in files:
+                    parts = os.path.splitext(file)
+                    if (len(parts) > 1) and (parts[1] == ".md"):
                         found_files = True
                         break
 
-                if not found_files:
-                    msg = "missing book: '{0}'".format(link)
-                    self.log.warnings.append(msg)
-                    App.logger.debug(msg)
+                if found_files:
+                    break
+
+            if not found_files:
+                msg = "missing book: '{0}'".format(link)
+                self.log.warnings.append(msg)
+                App.logger.debug(msg)
 
         return super(TqLinter, self).lint()  # Runs checks on Markdown, using the markdown linter
 
