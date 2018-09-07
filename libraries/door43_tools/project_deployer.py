b5d4f1b2489af15e6616db3ce1e223bad70c5884
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2018-09-07 14:34:15 -0400

    Added better status messages and remove uneeded code

--- libraries/door43_tools/project_deployer.py
+++ libraries/door43_tools/project_deployer.py
@@ -96,7 +96,7 @@ class ProjectDeployer(object):
         resource_type = build_log['resource_type']
         template_key = 'templates/project-page.html'
         template_file = os.path.join(template_dir, 'project-page.html')
-        App.logger.debug("Downloading {0} to {1}...".format(template_key, template_file))
+        App.logger.debug("Downloading {0} to {1}...".format(App.cdn_bucket + "/" + template_key, template_file))
         App.door43_s3_handler().download_file(template_key, template_file)
 
         if not do_multipart_merge:
@@ -307,10 +307,8 @@ class ProjectDeployer(object):
 
     @staticmethod
     def redeploy_all_projects(deploy_function, ignoretime = False):
-        i = 0
         one_day_ago = datetime.utcnow() - timedelta(hours=24)
         for obj in App.cdn_s3_handler().get_objects(prefix='u/', suffix='build_log.json'):
-            i += 1
             last_modified = obj.last_modified.replace(tzinfo=None)
             if one_day_ago <= last_modified and not ignoretime:
                 continue
