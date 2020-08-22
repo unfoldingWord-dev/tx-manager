525f423ccfcda94e7369db601c327cdf12a0e138
Russell Aunger <rba@live.com>
2020-08-22 15:40:02 -0400

    Default to current AWS region.

--- libraries/aws_tools/s3_handler.py
+++ libraries/aws_tools/s3_handler.py
@@ -9,7 +9,7 @@ from libraries.general_tools.file_utils import get_mime_type
 
 class S3Handler(object):
     def __init__(self, bucket_name=None, aws_access_key_id=None, aws_secret_access_key=None,
-                 aws_region_name='us-west-2'):
+                 aws_region_name=None):
         self.bucket_name = bucket_name
         self.aws_access_key_id = aws_access_key_id
         self.aws_secret_access_key = aws_secret_access_key
